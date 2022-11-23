8bd2eddfe8ffb92bc76fb887f3bac89ca7fb4e4e
Kevin <kevin.zhang@deeperse.com>
2022-11-23 11:05:07 +0800

    conf

---
+++ conf/conf.go
@@ -0,0 +1,13 @@
+package conf
+
+var (
+	LenStackBuf     = 4096
+	LogLevel        string
+	LogPath         string
+	ConsolePort     int
+	ConsolePrompt   = "Forest# "
+	ProfilePath     string
+	ListenAddr      string
+	ConnAddrs       []string
+	PendingWriteNum int
+)
