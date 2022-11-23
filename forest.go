6ff3180553a2cd418bc2db064b3d1fb9b8f35759
Kevin <kevin.zhang@deeperse.com>
2022-11-23 16:35:25 +0800

    forest

---
+++ forest.go
@@ -0,0 +1,37 @@
+package forest
+
+import (
+	"github.com/choutsugi/forest/conf"
+	"github.com/choutsugi/forest/log"
+	"github.com/choutsugi/forest/module"
+	"os"
+	"os/signal"
+)
+
+func Run(mods ...module.Module) {
+	// logger
+	if conf.LogLevel != "" {
+		logger, err := log.New(conf.LogLevel, conf.LogPath)
+		if err != nil {
+			panic(err)
+		}
+		log.Export(logger)
+		defer logger.Close()
+	}
+
+	log.Release("Leaf %v starting up", version)
+
+	// module
+	for i := 0; i < len(mods); i++ {
+		module.Register(mods[i])
+	}
+	module.Init()
+
+	// close
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	sig := <-c
+	log.Release("Leaf closing down (signal: %v)", sig)
+
+	module.Destroy()
+}
---
+++ version.go
@@ -0,0 +1,3 @@
+package forest
+
+const version = "0.0.1"
