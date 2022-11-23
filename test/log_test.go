adac95ac43fb12d8146e037bac54a2c8e8617796
Kevin <kevin.zhang@deeperse.com>
2022-11-23 15:08:08 +0800

    log test

---
+++ test/log_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"github.com/choutsugi/forest/log"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	name := "Forest"
+
+	log.Debug("My name is %v", name)
+	log.Release("My name is %v", name)
+	log.Error("My name is %v", name)
+	// log.Fatal("My name is %v", name)
+
+	logger, err := log.New("release", "")
+	if err != nil {
+		return
+	}
+	defer logger.Close()
+
+	logger.Debug("will not print")
+	logger.Release("My name is %v", name)
+
+	log.Export(logger)
+
+	log.Debug("will not print")
+	log.Release("My name is %v", name)
+}
